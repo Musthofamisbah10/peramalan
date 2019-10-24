71775715467a1c19c489fc9b99cf7ac2617ea1d0
Musthofamisbah10 <musthofaisbah09@gmail.com>
2019-10-24 21:05:33 +0700

    kategori

--- application/modules/kategori/controllers/Kategori.php
+++ application/modules/kategori/controllers/Kategori.php
@@ -3,12 +3,80 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Kategori extends CI_Controller {
 
+	public function __construct()
+	{	
+		parent::__construct();
+		$this->load->model('kategori/kategori_model');
+	}
+
 	public function index()
 	{
-		$data['judul'] = 'Peramalan Penjualan';
+		$data['judul'] 		= 'Peramalan Penjualan';
+		$data['kategori'] 	= $this->kategori_model->getAllKategori();
+
 		$this->load->view('templates/header', $data);
 		$this->load->view('templates/sidebar');
-		$this->load->view('kategori/kategori');
+		$this->load->view('kategori/kategori', $data);
 		$this->load->view('templates/footer');
+	}
+
+	public function add()
+	{
+		$kategori = $this->kategori_model;
+		$validation = $this->form_validation;
+		$validation->set_rules($kategori->_rules());
+
+		if ($validation->run()) {
+			$kategori->save();
+			$this->session->set_flashdata('msg', '<div class="alert alert-success
+				alert-dismissible
+				fade show" role="alert">
+				Data ketgori berhasil ditambah
+				<button type="button" class="close" data-dismiss="alert" aria-label="Close">
+				<span aria-hidden="true">&times;</span>
+				</button>
+				</div>');
+			redirect('kategori');
+		}else{
+			redirect('kategori');
+		}
+	}
+
+	public function update($id=null)
+	{
+		$kategori = $this->kategori_model;
+		$validation = $this->form_validation;
+		$validation->set_rules($kategori->_rules());
+
+		if ($validation->run()) {
+			$kategori->update();
+			$this->session->set_flashdata('msg', '<div class="alert alert-success
+				alert-dismissible
+				fade show" role="alert">
+				Data ketgori berhasil diubah
+				<button type="button" class="close" data-dismiss="alert" aria-label="Close">
+				<span aria-hidden="true">&times;</span>
+				</button>
+				</div>');
+			redirect('kategori');
+		}else{
+			redirect('kategori');
+		}
+	}
+
+	public function delete($id=null)
+	{
+		$kategori = $this->kategori_model;
+		if ($kategori->delete($id)) {
+			$this->session->set_flashdata('msg', '<div class="alert alert-danger
+				alert-dismissible
+				fade show" role="alert">
+				Data facilities berhasil dihapus
+				<button type="button" class="close" data-dismiss="alert" aria-label="Close">
+				<span aria-hidden="true">&times;</span>
+				</button>
+				</div>');
+			redirect('kategori');
+		}
 	}
 }
--- application/modules/kategori/views/kategori.php
+++ application/modules/kategori/views/kategori.php
@@ -4,14 +4,12 @@
         <div class="row">
             <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
                 <div class="page-header">
-                    <h2 class="pageheader-title">Blank Pageheader </h2>
-                    <p class="pageheader-text">Proin placerat ante duiullam scelerisque a velit ac porta, fusce sit amet vestibulum mi. Morbi lobortis pulvinar quam.</p>
+                    <h2 class="pageheader-title">Data Kategori </h2>
                     <div class="page-breadcrumb">
                         <nav aria-label="breadcrumb">
                             <ol class="breadcrumb">
-                                <li class="breadcrumb-item"><a href="#" class="breadcrumb-link">Dashboard</a></li>
-                                <li class="breadcrumb-item"><a href="#" class="breadcrumb-link">Pages</a></li>
-                                <li class="breadcrumb-item active" aria-current="page">Blank Pageheader</li>
+                                <li class="breadcrumb-item"><a href="<?= base_url('dashboard') ?>" class="breadcrumb-link">Dashboard</a></li>
+                                <li class="breadcrumb-item active" aria-current="page">Data Kategori</li>
                             </ol>
                         </nav>
                     </div>
@@ -22,69 +20,31 @@
         <!-- content -->
         <div class="row">
             <div class="col-md-12 col-sm-12 col-12">
+                <?= $this->session->flashdata('msg') ?>
                 <div class="card">
-                    <h5 class="card-header">Striped Table</h5>
                     <div class="card-body">
-                        <button class="btn btn-primary btn-sm float-right mb-2" data-toggle="modal" data-target="#exampleModal"><i class="fas fa-plus"></i> Tambah Data</button>
+                        <button class="btn btn-primary btn-sm float-right mb-2" data-toggle="modal" data-target="#addModal"><i class="fas fa-plus"></i> Tambah Data</button>
                         <table class="table table-striped">
                             <thead>
                                 <tr>
                                     <th>No</th>
-                                    <th>First</th>
-                                    <th>Last</th>
-                                    <th>Handle</th>
-                                    <th colspan="2">action</th>
+                                    <th>Nama Kategori</th>
+                                    <th colspan="2">Action</th>
                                 </tr>
                             </thead>
                             <tbody>
-                                <tr>
-                                    <th scope="row">1</th>
-                                    <td>Mark</td>
-                                    <td>Otto</td>
-                                    <td>@mdo</td>
-                                    <td width="2px">
-                                        <a href="#" class="badge badge-pill badge-danger"><i class="fas fa-trash"></i> Delete</a>
-                                    </td>
-                                    <td width="2px">
-                                        <a href="#" class="badge badge-pill badge-warning"><i class="fas fa-edit"></i> Edit</a>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <th scope="row">1</th>
-                                    <td>Mark</td>
-                                    <td>Otto</td>
-                                    <td>@mdo</td>
-                                    <td width="2px">
-                                        <a href="#" class="badge badge-pill badge-danger"><i class="fas fa-trash"></i> Delete</a>
-                                    </td>
-                                    <td width="2px">
-                                        <a href="#" class="badge badge-pill badge-warning"><i class="fas fa-edit"></i> Edit</a>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <th scope="row">1</th>
-                                    <td>Mark</td>
-                                    <td>Otto</td>
-                                    <td>@mdo</td>
-                                    <td width="2px">
-                                        <a href="#" class="badge badge-pill badge-danger"><i class="fas fa-trash"></i> Delete</a>
-                                    </td>
-                                    <td width="2px">
-                                        <a href="#" class="badge badge-pill badge-warning"><i class="fas fa-edit"></i> Edit</a>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <th scope="row">1</th>
-                                    <td>Mark</td>
-                                    <td>Otto</td>
-                                    <td>@mdo</td>
-                                    <td width="2px">
-                                        <a href="#" class="badge badge-pill badge-danger"><i class="fas fa-trash"></i> Delete</a>
-                                    </td>
-                                    <td width="2px">
-                                        <a href="#" class="badge badge-pill badge-warning"><i class="fas fa-edit"></i> Edit</a>
-                                    </td>
-                                </tr>
+                                <?php $no = 1; foreach ($kategori as $row): ?>
+                                    <tr>
+                                        <th width="2px"><?= $no++ ?></th>
+                                        <td><?= $row->nama_kategori ?></td>
+                                        <td width="2px">
+                                            <a href="<?= site_url('kategori/delete/'. $row->id_kategori) ?>" class="badge badge-pill badge-danger"><i class="fas fa-trash"></i> Delete</a>
+                                        </td>
+                                        <td width="2px">
+                                            <a href="<?= site_url('kategori/update/'. $row->id_kategori) ?>" class="badge badge-pill badge-warning" data-toggle="modal" data-target="#editModal<?=$row->id_kategori?>"><i class="fas fa-edit"></i> Edit</a>
+                                        </td>
+                                    </tr>
+                                <?php endforeach; ?>
                             </tbody>
                         </table>
                     </div>
@@ -94,30 +54,63 @@
         <!-- and content -->
     </div>
 
-    <!-- Modal Kategori -->
-   <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <!-- Add Modal Kategori -->
+    <div class="modal fade" id="addModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
         <div class="modal-dialog" role="document">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                    <h5 class="modal-title" id="exampleModalLabel">Tambah Kategori</h5>
                     <a href="#" class="close" data-dismiss="modal" aria-label="Close">
                         <span aria-hidden="true">&times;</span>
                     </a>
                 </div>
                 <div class="modal-body">
-                    <form id="form" data-parsley-validate="" novalidate="">
+                    <form id="form" action="<?= base_url('kategori/add') ?>" method="post">
                         <div class="form-group row">
-                            <label for="inputEmail2" class="col-3 col-lg-2 col-form-label text-right">Stock</label>
+                            <label for="nama_kategori" class="col-3 col-lg-2 col-form-label text-right">Kategori</label>
                             <div class="col-9 col-lg-10">
-                                <input id="inputEmail2" type="email" required="" data-parsley-type="email" placeholder="Email" class="form-control">
+                                <input id="nama_kategori" type="text" name="nama_kategori" required="" data-parsley-type="nama_kategori" placeholder="Masukan Kategori" class="form-control">
                             </div>
+                            <?php echo form_error('name_kategori', '<div class="text-danger small ml-3">') ?>
                         </div>
                     </div>
                     <div class="modal-footer">
-                        <a href="#" class="btn btn-secondary" data-dismiss="modal">Keluar</a>
-                        <a href="#" class="btn btn-primary">Tambah Data</a>
+                        <button class="btn btn-secondary" data-dismiss="modal">Batal</button>
+                        <button type="submit" class="btn btn-primary">Tambah Data</button>
                     </div>
                 </form>
             </div>
         </div>
     </div>
+
+    <!-- Update Modal Kategori -->
+    <?php foreach ($kategori as $row): ?>
+    <div class="modal fade" id="editModal<?=$row->id_kategori?>" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Ediat Kategori</h5>
+                    <a href="#" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </a>
+                </div>
+                <div class="modal-body">
+                    <form id="form" action="<?= base_url('kategori/update') ?>" method="post">
+                        <input type="hidden" class="form-control" name="id_kategori" value="<?= $row->id_kategori ?>">
+                        <div class="form-group row">
+                            <label for="nama_kategori" class="col-3 col-lg-2 col-form-label text-right">Kategori</label>
+                            <div class="col-9 col-lg-10">
+                                <input id="nama_kategori" type="text" name="nama_kategori" required="" data-parsley-type="nama_kategori" value="<?= $row->nama_kategori ?>" class="form-control">
+                            </div>
+                            <?php echo form_error('name_kategori', '<div class="text-danger small ml-3">') ?>
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button class="btn btn-secondary" data-dismiss="modal">Batal</button>
+                        <button type="submit" class="btn btn-primary">Ubah Data</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+    <?php endforeach; ?>
